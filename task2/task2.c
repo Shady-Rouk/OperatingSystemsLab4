9d8c4cfaba19ed49d19e244a67d93c74ed8cecf6
Farouk Balogun <farouk.balogun@bison.howard.edu>
2022-10-31 20:36:17 +0000

    Pushing solution for Lab 4 Tasks 1&2

---
+++ task2/task2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* IMPLEMENT ME: Declare your functions here */
+int add (int a, int b);
+int subtract (int a, int b);
+int multiply (int a, int b);
+int divide (int a, int b);
+
+
+int main (void)
+{
+	/* IMPLEMENT ME: Insert your algorithm here */
+	int (*func_arr[])(int a, int b) = {add, subtract, multiply, divide};
+	int a, b;
+	a = 6;
+	b = 3;
+	// array of function pointers?
+	// read user input
+	// while user_input != 4
+	// evaulate array of function pointers[user_input]
+	int user_op;
+	user_op = -1;
+	
+	printf("Operand 'a': %d | Operand 'b': %d\n", a, b);
+	printf("Specify the operation to perform (0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4 : exit): ");
+	scanf("%d", &user_op);
+	while (user_op != 4) {
+		printf("x=%d\n", (*func_arr[user_op])(a, b));
+		printf("Operand 'a': %d | Operand 'b': %d\n", a, b);
+		printf("Specify the operation to perform (0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4 : exit): ");
+		scanf("%d", &user_op);
+	}
+	
+	return 0;
+}
+
+/* IMPLEMENT ME: Define your functions here */
+int add (int a, int b) { printf ("Adding 'a' and 'b'\n"); return a + b; }
+int subtract (int a, int b) { printf ("Subtracting 'b' from 'a'\n"); return a - b; }
+int multiply (int a, int b) { printf ("Multiplying 'a' and 'b'\n"); return a * b; }
+int divide (int a, int b) { printf ("Dividing 'a' by 'b'\n"); return a / b; }
